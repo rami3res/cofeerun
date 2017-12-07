48d81e3c15c6b3ccf0453dfc6315190b65dbdfb5
ramieres <urami3res@gmail.com>
2017-12-07 19:04:01 +0300

    first commit

---
+++ scripts/truck.js
@@ -0,0 +1,31 @@
+(function(window) {
+  'use strict';
+  var App = window.App || {};
+
+  function Truck(truckID, db) {
+    this.truckID = truckID;
+    this.db = db;
+  }
+
+  Truck.prototype.createOrder = function(order) {
+    console.log('Adding order for ' + order.emailAddress);
+    this.db.add(order.emailAddress, order);
+  };
+
+  Truck.prototype.deliverOrder = function(customerID) {
+    console.log('Delivering order for ' + customerID);
+    this.db.remove(customerID);
+  };
+
+  Truck.prototype.printOrders = function() {
+    var customerIdArray = Object.keys(this.db.getAll());
+
+    console.log('Truck #' + this.truckID + ' has pending orders:');
+    customerIdArray.forEach(function(id) {
+      console.log(this.db.get(id));
+    }.bind(this));
+  };
+
+  App.Truck = Truck;
+  window.App = App;
+})(window);
